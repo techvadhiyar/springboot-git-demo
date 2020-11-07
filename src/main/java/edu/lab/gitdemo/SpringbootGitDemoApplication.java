a6db0f738d4f8faa62b3c2f320c165a3ed8eec9e
cpccharry <harry.subramaniam@cpcc.edu>
2020-11-07 11:37:42 -0500

    First release.

---
+++ src/main/java/edu/lab/gitdemo/SpringbootGitDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.lab.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lab/gitdemo/SpringbootGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
